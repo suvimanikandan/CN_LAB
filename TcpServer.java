b643a7394a7645a9946fdf31b396c439760698e4
swetha_suvi <80690771+suvimanikandan@users.noreply.github.com>
2021-04-19 11:12:36 +0530

    Add files via upload

---
+++ TcpServer.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+class TcpServer {
+  public static void main(String args[]) {
+    try {
+ServerSocket socket = new ServerSocket(5604);
+System.out.println("ARP Server is up.");
+      Socket client = socket.accept();
+
+      while(true) {
+DataInputStream din = new DataInputStream(client.getInputStream());
+DataInputStream dout = new DataOutputStream(client.getOutputStream());
+
+        String str = din.readLine();
+        String ip[] = {"165.165.80.80", "165.165.79.1"};
+        String mac[] = {"6A:08:AA:C2", "8A:BC:E3:FA"};
+
+        for(int i = 0; i<ip.length; i++) {
+          if(str.equals(ip[i])) {
+dout.writeBytes(mac[i] + '\n');
+            break;
+          }
+        }
+socket.close();
+      }
+    } catch(NullPointerException e) {
+System.out.println("Shutting down server...");
+    } catch(Exception e) {
+System.err.println(e);
+    }
+  }
+}
+
